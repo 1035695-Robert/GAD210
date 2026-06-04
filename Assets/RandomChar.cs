75af752d9fcdd258336bc566029af2e63ba40945
Robert Andren - Home <1035695@student.sae.edu.au>
2026-06-04 15:56:11 +1000

    Updated Input and conditions

--- Assets/RandomChar.cs
+++ Assets/RandomChar.cs
@@ -1,34 +1,46 @@
 using JetBrains.Annotations;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using Unity.Collections;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class RandomChar : MonoBehaviour
 {
-    string listOfChar = "1234";
-    //abcdefghijklmnopqrstuvwxyz
-    [SerializeField] char[] charArray;
-    [SerializeField] float timerLength = 2;
+    string listOfChar = "qwertyuiopasdfghjklzxcvbnm";
+
+    [SerializeField] List<char> charArray;
+    public float defaultTimerLenght = 2;
+    public float timerLength;
+
+    public float inputCheckTimer;
+
     float waitTime = 0.5f;
 
     public TextMeshProUGUI displayKey;
+    public TextMeshProUGUI correctCounter;
+    public int bonusCounter = 0;
 
 
 
     void Start()
     {
-        charArray = listOfChar.ToCharArray();
+        foreach (char a in listOfChar)
+        {
+            charArray.Add(a);
+        }
+        timerLength = defaultTimerLenght;
         SelectChar();
     }
 
     private void SelectChar()
     {
-        int index = Random.Range(0, charArray.Length);
+        int index = Random.Range(0, charArray.Count);
         string selectedChar = charArray[index].ToString();
-        displayKey.text = selectedChar;
+        displayKey.text = selectedChar.ToUpper();
 
         StartCoroutine(CharLoop(selectedChar));
     }
@@ -36,39 +48,39 @@ public class RandomChar : MonoBehaviour
     IEnumerator CharLoop(string selectedChar)
     {
         Event.toggleOn.Invoke();
-        float inputCheckTimer = timerLength;
+        inputCheckTimer = timerLength;
 
         while (inputCheckTimer > 0f)
         {
             if (Keyboard.current.anyKey.wasPressedThisFrame)
             {
                 string pressedKey = Keyboard.current.allKeys.FirstOrDefault(k => k.wasPressedThisFrame).name;
-                foreach (var c in charArray)
+                Debug.Log(pressedKey);
+                foreach (char c in charArray)
                 {
                     if (pressedKey == c.ToString())
                     {
-                        Debug.Log(pressedKey);
+                        Debug.Log("pressed this key " + pressedKey);
                         if (pressedKey == selectedChar)
                         {
-                            Debug.Log("correct match");
-                            
+                            Correct();
+                            StartCoroutine(WaitBetweenInputs());
+                            yield break;
                         }
                         else
                         {
-                            Debug.LogError("Failed Match");
+                            Failed();
+                            StartCoroutine(WaitBetweenInputs());
+                            yield break;
                         }
                     }
-                    StartCoroutine(WaitBetweenInputs());
-                    yield break;
+                    yield return null;
                 }
-
-               
-
             }
-
             inputCheckTimer -= Time.deltaTime;
             yield return null;
         }
+        Failed();
         StartCoroutine(WaitBetweenInputs());
         yield break;
     }
@@ -78,5 +90,27 @@ public class RandomChar : MonoBehaviour
         Event.toggleOff.Invoke();
         yield return new WaitForSeconds(waitTime);
         SelectChar();
+    }
+    void Correct()
+    {
+        Debug.Log("correct match");
+        
+        timerLength -= 0.2f;
+        if (timerLength < 0.5f)
+        {
+            timerLength = 0.5f;
+        }
+
+        bonusCounter++;
+        correctCounter.text = "Combo: "+ bonusCounter.ToString() +"x";
+    }
+    void Failed()
+    {
+        Debug.Log("Failed Match");
+        charArray.RemoveAt(charArray.Count - 1);
+
+        timerLength = defaultTimerLenght;
+        bonusCounter = 0;
+        correctCounter.text = bonusCounter.ToString();
     }
 }
